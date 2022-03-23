98ae99c9ebefbf6aeb5a09ec2969cf6a55608ac9
xeiq <xeiq.ds@gmail.com>
2022-03-23 19:12:49 +0100

    usuniecie whitespaces z loginu i hasla"

--- helpers/read_password.py
+++ helpers/read_password.py
@@ -1,8 +1,8 @@
 def read_password():
     f = open("helpers/pass.txt", "r")
     lines = f.readlines()
-    username = lines[0]
-    password = lines[1]
+    username = lines[0].strip()
+    password = lines[1].strip()
     f.close()
 
     return username, password
